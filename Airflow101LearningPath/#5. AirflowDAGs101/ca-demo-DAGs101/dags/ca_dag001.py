252bbb29112f6c8ede7904242a9a1a585f52a7d3
cesaramehri <cesar.amehri@gmail.com>
2024-12-10 12:18:18 +1300

    Airflow Astro LP: Airflow DAGs 101

---
+++ Airflow101LearningPath/#5. AirflowDAGs101/ca-demo-DAGs101/dags/ca_dag001.py
@@ -0,0 +1,48 @@
+# Import Statements
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.python import PythonOperator
+from airflow.utils.helpers import chain
+
+# Define your UDFs
+def print_1():
+    print('Hello from taks_1')
+def print_2():
+    print('Hello from taks_2')
+def print_3():
+    print('Hello from taks_3')
+def print_4():
+    print('Hello from taks_4')
+def print_5():
+    print('Hello from taks_5')
+
+# Define your DAG Object
+with DAG('ca_dag001',
+         start_date = datetime(2024,12,10),
+         description = 'First DAG',
+         tags = ['DEs'],
+         schedule = '@daily',
+         catchup = False 
+    ):
+    
+    # Create your tasks
+    task_1 = PythonOperator(task_id = 'task_1',
+                            python_callable = print_1
+                        )
+    task_2 = PythonOperator(task_id = 'task_2',
+                            python_callable = print_2
+                        )
+    task_3 = PythonOperator(task_id = 'task_3',
+                            python_callable = print_3
+                        )
+    task_4 = PythonOperator(task_id = 'task_4',
+                            python_callable = print_4
+                        )
+    task_5 = PythonOperator(task_id = 'task_5',
+                            python_callable = print_5
+                        )
+    
+    # Define Orchestration Logic (task 1 then (task 2 and task 3 in parallel), then task 4)
+    #task_1 >> [task_2, task_3, task_4] >> task_5
+
+    chain(task_1, [task_2, task_3], [task_4, task_5])
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # caAirflowProjects
+
+## Airflow 101 Learning Path
+All project materials and tutorials can be found in: https://academy.astronomer.io/path/airflow-101 
